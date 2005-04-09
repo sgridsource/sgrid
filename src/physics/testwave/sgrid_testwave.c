ebeadfeced17869ddad26c7883099b030ae2839b
wolf <wolf>
2005-04-09 21:58:20 +0000

    better

--- src/physics/testwave/sgrid_testwave.c
+++ src/physics/testwave/sgrid_testwave.c
@@ -11,15 +11,18 @@ int sgrid_testwave()
 
   /* functions */
   AddFun(INITIALDATA, testwave_startup, "initialize test");
-  AddFun(ANALYZE, testwave_analyze, "compute test results");
-  AddFun(POST_EVOLVE, testwave_filter, "filter u and k");
+  // AddFun(ANALYZE, testwave_analyze, "compute test results");
+  // AddFun(POST_EVOLVE, testwave_filter, "filter u and k");
 
   /* variables */
   AddVar("testwave_u", "",  "wave function");
   AddVar("testwave_k", "",  "time deriv of wave function");
-  AddVar("testwave_u11", "", "derivs of wave function");
-  AddVar("testwave_u22", "", "derivs of wave function");
-  AddVar("testwave_u33", "", "derivs of wave function");
+  AddVar("testwave_u1", "", "derivs of wave function");
+  AddVar("testwave_u2", "", "derivs of wave function");
+  AddVar("testwave_u3", "", "derivs of wave function");
+  AddVar("testwave_u11", "", "2nd derivs of wave function");
+  AddVar("testwave_u22", "", "2nd derivs of wave function");
+  AddVar("testwave_u33", "", "2nd derivs of wave function");
   AddVar("testwave_dum", "", "dummy var");
   
   /* parameters */
