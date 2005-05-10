1d23a2a2b6c83f1c62fc93975e848d031b88e3c9
wolf <wolf>
2005-05-10 21:22:36 +0000

    more macros

--- src/main/MemoryMan/sgrid_MemoryMan_loops.h
+++ src/main/MemoryMan/sgrid_MemoryMan_loops.h
@@ -1,18 +1,20 @@
 /* sgrid_MemoryMan_loops.h */
-/* Wolfgang Tichy, April 2005  &  Bernd Bruegmann, 12/99, 10/02 */
+/* Wolfgang Tichy, April 2005 */
 
 /* Loops are performed by macros so that the user has to know very little
    about the implementation details.
-
 */
-
-
 
 /****************************************************************************/
-/* for all points without stencil:
-   for all is simple because this is how we store them 
-   the name of the index is passed to the macro 
-*/
+/* loop over all points in a box */
 #define forallpoints(box,i) \
   for (i = 0; i < box->nnodes; i++)
 
+/* loop over all points in all boxes */
+#define forallgridpoints(grid,box,boxindex,ijk) \
+  for(box=grid->box[boxindex], boxindex=0; boxindex<grid->nboxes; boxindex++) \
+    for(ijk=0; ijk<box->nnodes; ijk++)
+
+/* loop over all boxes */
+#define forallboxes(grid,boxindex) \
+  for(boxindex=0; boxindex < grid->nboxes; boxindex++)
