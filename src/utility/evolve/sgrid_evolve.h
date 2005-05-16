616a716c9dd619117f2467094e61baa9450cf9d9
wolf <wolf>
2005-05-16 21:20:32 +0000

    forgotten def

--- src/utility/evolve/sgrid_evolve.h
+++ src/utility/evolve/sgrid_evolve.h
@@ -4,3 +4,5 @@
 void evolve_rhsregister(void (*f)(tVarList *, tVarList *, double, tVarList *));
 void evolve_vlregister(tVarList *u);
 void evolve_vlretrieve(tVarList **vlu_c, tVarList **vlu_p, tVarList **vlu_pp);
+void addDissipation(tVarList *vlu_n, tVarList *vlu_p, double dt, 
+                    tVarList *vlu_c);
