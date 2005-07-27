79bed5840c2b6d6ccb68f5c5733a5fd1f741134d
wolf <wolf>
2005-07-27 17:48:04 +0000

    some functions to get all spectral coeffs

---
+++ src/utility/Spectral/spec_coeffs.c
@@ -0,0 +1,176 @@
+/* spec_coeffs.c */
+/* Wolfgang Tichy 7/2005 */
+
+#include "sgrid.h"
+#include "Spectral.h"
+
+
+
+/* init a n1*n1 matrix M used to compute coeffs */
+void initMatrix_ForCoeffs(double *M, int n1,
+                          void (*get_coeffs)(double *,double *, int))
+{
+  int i,j;
+  double *u;
+  double *c;
+
+  u = (double *) calloc(n1, sizeof(double));
+  c = (double *) calloc(n1, sizeof(double));
+
+  if( !(u && c) ) errorexit("initMatrix_ForCoeffs: out of memory for u, c");
+
+
+  /* read matrix from functions */
+  for(j=0; j<n1; j++)
+  {
+    u[j]=1.0;
+
+    get_coeffs(c, u, n1-1);
+    
+    /* set M */
+    for(i=0; i<n1; i++) M[n1*i + j] = c[i];
+    
+    u[j]=0.0;
+  }
+  free(u);
+  free(c);
+}
+
+/* init a n1*n1 matrix M used to compute function u from coeffs */
+void initMatrix_ToEvaluate(double *M, int n1,
+                           void (*eval_onPoints)(double *,double *, int))
+{
+  int i,j;
+  double *u;
+  double *c;
+
+  u = (double *) calloc(n1, sizeof(double));
+  c = (double *) calloc(n1, sizeof(double));
+
+  if( !(u && c) ) errorexit("initMatrix_ForCoeffs: out of memory for u, c");
+
+
+  /* read matrix from functions */
+  for(j=0; j<n1; j++)
+  {
+    c[j]=1.0;
+
+    eval_onPoints(c, u, n1-1);
+    
+    /* set M */
+    for(i=0; i<n1; i++) M[n1*i + j] = u[i];
+    
+    c[j]=0.0;
+  }
+  free(u);
+  free(c);
+}
+
+
+
+/* get the coeffs c of 3d var u in direction direc on the whole box */
+void spec_analysis1(tBox *box, int direc, double *M, double *u, double *c)
+{
+  static int linelen=0;
+  static double *uline=NULL;
+  static double *cline=NULL;
+  int i,j,k, m3;
+    
+  /* static memory for lines */
+  m3=max3(box->n1, box->n2, box->n3);
+  if(m3>linelen)
+  {
+    linelen = m3;
+    uline = (double*) realloc(uline, linelen * sizeof(double));
+    cline = (double*) realloc(cline, linelen * sizeof(double));
+  }
+
+  if(direc==1)
+  {
+    for (k = 0; k < box->n3; k++)
+      for (j = 0; j < box->n2; j++)
+      {
+        get_memline(u, uline, 1, j, k, box->n1, box->n2, box->n3);
+        matrix_times_vector(M, uline, cline, box->n1);
+        put_memline(c, cline, 1, j, k, box->n1, box->n2, box->n3);        
+      }
+  }
+  else if(direc==2)
+  {
+    for (k = 0; k < box->n3; k++)
+      for (i = 0; i < box->n1; i++)
+      {
+        get_memline(u, uline, 2, i, k, box->n1, box->n2, box->n3);
+        matrix_times_vector(M, uline, cline, box->n2);
+        put_memline(c, cline, 2, i, k, box->n1, box->n2, box->n3);        
+      }
+  }
+  else if(direc==3)
+  {
+    for (j = 0; j < box->n2; j++)
+      for (i = 0; i < box->n1; i++)
+      {
+        get_memline(u, uline, 3, i, j, box->n1, box->n2, box->n3);
+        matrix_times_vector(M, uline, cline, box->n3);
+        put_memline(c, cline, 3, i, j, box->n1, box->n2, box->n3);
+      }
+  }
+  else
+    errorexit("spec_analysis1: possible values for direction direc are 1,2,3.");
+}
+
+
+
+
+/* compute the 3d var u from the coeffs c in direction direc  
+   on the whole box                                         */
+void spec_synthesis1(tBox *box, int direc, double *M, double *u, double *c)
+{
+  static int linelen=0;
+  static double *uline=NULL;
+  static double *cline=NULL;
+  int i,j,k, m3;
+    
+  /* static memory for lines */
+  m3=max3(box->n1, box->n2, box->n3);
+  if(m3>linelen)
+  {
+    linelen = m3;
+    uline = (double*) realloc(uline, linelen * sizeof(double));
+    cline = (double*) realloc(cline, linelen * sizeof(double));
+  }
+
+  if(direc==1)
+  {
+    for (k = 0; k < box->n3; k++)
+      for (j = 0; j < box->n2; j++)
+      {
+        get_memline(c, cline, 1, j, k, box->n1, box->n2, box->n3);
+        matrix_times_vector(M, uline, cline, box->n1);
+        put_memline(u, uline, 1, j, k, box->n1, box->n2, box->n3);        
+      }
+  }
+  else if(direc==2)
+  {
+    for (k = 0; k < box->n3; k++)
+      for (i = 0; i < box->n1; i++)
+      {
+        get_memline(c, cline, 2, i, k, box->n1, box->n2, box->n3);
+        matrix_times_vector(M, uline, cline, box->n2);
+        put_memline(u, uline, 2, i, k, box->n1, box->n2, box->n3);        
+      }
+  }
+  else if(direc==3)
+  {
+    for (j = 0; j < box->n2; j++)
+      for (i = 0; i < box->n1; i++)
+      {
+        get_memline(c, cline, 3, i, j, box->n1, box->n2, box->n3);
+        matrix_times_vector(M, uline, cline, box->n3);
+        put_memline(u, uline, 3, i, j, box->n1, box->n2, box->n3);
+      }
+  }
+  else
+    errorexit("spec_synthesis1: possible values for direction direc are 1,2,3.");
+}
+
