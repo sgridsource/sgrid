5268a3af4372eb8f16a12a08e5e374e94055ea58
wolf <wolf>
2005-07-27 13:48:33 +0000

    use average at double covered points

--- src/utility/Coordinates/doubleCovering.c
+++ src/utility/Coordinates/doubleCovering.c
@@ -36,6 +36,7 @@ void reset_doubleCoveredPoints_SphericalDF(tBox *box, tVarList *unew)
   int vi;
   int i,j,k, n1,n2,n3;
   double *u;
+  double av;
 
   /* for all variables */
   for(vi = 0; vi < unew->n; vi++)
@@ -50,6 +51,7 @@ void reset_doubleCoveredPoints_SphericalDF(tBox *box, tVarList *unew)
                 "n2 and n3 must be even!");
 
     /* copy points into double covered regions */
+    /*
     for(k = 0; k < n3/2; k++)
       for(j = n2/2; j < n2; j++)
         for(i = 0; i < n1; i++)
@@ -59,5 +61,24 @@ void reset_doubleCoveredPoints_SphericalDF(tBox *box, tVarList *unew)
       for(j = n2/2; j < n2; j++)
         for(i = 0; i < n1; i++)
           u[Index(i,j,k)] = u[Index(i,n2-j-1,k-n3/2)];
+    */
+    /* use average of u at the double covered points */
+    for(k = 0; k < n3/2; k++)
+      for(j = n2/2; j < n2; j++)
+        for(i = 0; i < n1; i++)
+        {
+          av = ( u[Index(i,j,k)] + u[Index(i,n2-j-1,k+n3/2)] )*0.5;
+          u[Index(i,j,k)] = av;
+          u[Index(i,n2-j-1,k+n3/2)] = av;;
+        }
+
+    for(k = n3/2; k < n3; k++)
+      for(j = n2/2; j < n2; j++)
+        for(i = 0; i < n1; i++)
+        {
+          av = ( u[Index(i,j,k)] + u[Index(i,n2-j-1,k-n3/2)] )*0.5;
+          u[Index(i,j,k)] = av;
+          u[Index(i,n2-j-1,k-n3/2)] = av;
+        }
   }
 }
