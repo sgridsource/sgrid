bf2782a1ebbda40f3bcb2d4a6304e2f2a298ad81
wolf <wolf>
2007-08-18 00:42:26 +0000

    making standard four coeffs smaller by 1/N

--- src/utility/Spectral/explicit_Four_trafos.c
+++ src/utility/Spectral/explicit_Four_trafos.c
@@ -41,10 +41,72 @@ void four_deriv(double a, double b, double c[], double cder[], int n)
 }
 
 
-
 /* compute Four coeffs c[0...n] from function u 
    at x_k = k/N, k=0,...,N-1 , N=n+1 */
 void four_coeffs(double c[], double u[], int n)
+{
+  int k, j;
+  double Re_c_j, Im_c_j, PI2oN, ooN;
+  int N=n+1;
+
+  ooN=1.0/N;
+  PI2oN=2.0*PI*ooN;
+
+  /* first j=0 , i.e. c_0 */
+  c[0] = 0.0;
+  for(k=0;k<N;k++)  c[0] += u[k];
+  c[0]*=ooN;
+
+  for(j=1; j<=N/2; j++)
+  {
+    Re_c_j = Im_c_j = 0.0;
+    for(k=0;k<N;k++)
+    {
+      Re_c_j += cos(j*PI2oN*k)*u[k];
+      Im_c_j += sin(j*PI2oN*k)*u[k];
+    }
+    c[2*j-1] = Re_c_j * ooN; /* real part of c_j */
+    if(2*j<N)
+      c[2*j] = Im_c_j * ooN; /* imaginary part of c_j */
+  }
+  /* we should use a FFT for everything above this line */
+}
+
+
+/* find function u from Four coeffs c[0...n], computed with four_coeffs */
+void four_eval(double c[], double u[], int n)
+{
+  int k, j;
+  double sum, Re_c_k, Im_c_k, PI2oN;
+  int N=n+1;
+
+  PI2oN=2.0*PI/N;
+  
+  for(j=0; j<N; j++)
+  {
+    sum = 0.0;
+    for(k=1;k<=N/2;k++)
+    {
+      Re_c_k   = c[2*k-1];
+      if(2*k<N)
+        Im_c_k = c[2*k];
+      else
+        Im_c_k = 0.0;
+      sum += cos(k*PI2oN*j)*Re_c_k + sin(k*PI2oN*j)*Im_c_k;
+    }
+    if( N%2 == 0 )
+      u[j] = ( c[0] + 2.0*sum - c[N-1]*cos(PI*j) );
+    else
+      u[j] = ( c[0] + 2.0*sum );
+  }
+  /* we should use a FFT for eveything above this line */
+}
+
+
+/* compute Four coeffs c[0...n] from function u 
+   at x_k = k/N, k=0,...,N-1 , N=n+1 
+NOTE: four_coeffsN returns c[] that are N times of those of four_coeffs */
+void four_coeffsN(double c[], double u[], int n)
 {
   int k, j;
   double Re_c_j, Im_c_j, PI2oN;
@@ -72,9 +134,8 @@ void four_coeffs(double c[], double u[], int n)
 }
 
 
-
-/* find function u from Four coeffs c[0...n] */
-void four_eval(double c[], double u[], int n)
+/* find function u from Four coeffs c[0...n], computed with four_coeffsN */
+void four_evalN(double c[], double u[], int n)
 {
   int k, j;
   double sum, Re_c_k, Im_c_k, PI2oN;
