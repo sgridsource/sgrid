bc113bf9c69198e5e89531a35e5a52ccec5c4c4d
wolf <wolf>
2007-12-14 15:52:59 +0000

    let LinSolve_withUMFPACK use a column matrix

--- src/physics/Poisson/LinSolve_withUMFPACK.c
+++ src/physics/Poisson/LinSolve_withUMFPACK.c
@@ -6,7 +6,7 @@
 #include "Poisson.h"
 
 
-int LinSolve_withUMFPACK(tVarList *x, tVarList *b, 
+int LinSolve_withUMFPACK_old(tVarList *x, tVarList *b, 
             tVarList *r, tVarList *c1,tVarList *c2,
 	    int itmax, double tol, double *normres,
 	    void (*lop)(tVarList *, tVarList *, tVarList *, tVarList *), 
@@ -41,6 +41,46 @@ int LinSolve_withUMFPACK(tVarList *x, tVarList *b,
   /* free matrix Aline */
   for(line=0; line<nlines; line++)  FreeSparseVector(Aline[line]);
   free(Aline);
+
+  return INFO;
+}
+
+/* call all relevant routines to set up an umfpack call */
+int LinSolve_withUMFPACK(tVarList *x, tVarList *b, 
+            tVarList *r, tVarList *c1,tVarList *c2,
+	    int itmax, double tol, double *normres,
+	    void (*lop)(tVarList *, tVarList *, tVarList *, tVarList *), 
+	    void (*precon)(tVarList *, tVarList *, tVarList *, tVarList *))
+{
+  tGrid *grid = b->grid;
+  int bi;
+  int pr = 1;
+  int col, ncols, nvars, INFO;
+  tSparseVector **Acol;
+
+  if(pr) printf("LinSolve_withUMFPACK: ");
+
+  /* allocate Acol */
+  nvars=x->n; 
+  ncols = 0;
+  forallboxes(grid,bi)  ncols+=(grid->box[bi]->nnodes)*nvars;
+  Acol = calloc(ncols, sizeof(*Acol));
+  if(Acol)  if(pr) printf("allocated %d matrix columns\n", ncols);
+  else       errorexit("no memory for Acol");
+  for(col=0; col<ncols; col++)  Acol[col]=AllocateSparseVector();
+
+  /* set Acol */                
+  SetMatrixColumns_slowly(Acol, lop, r, x, c1, c2, pr);
+  if(pr&&0) 
+    for(col=0; col<ncols; col++) prSparseVector(Acol[col]);
+
+  /* solve A x = b with umfpack */
+  INFO=umfpack_solve_fromAcolumns(Acol, x, b, 1e-12, pr);
+  if(pr) printf("LinSolve_withUMFPACK: umfpack_solve returned INFO=%d\n",INFO);
+
+  /* free matrix Acol */
+  for(col=0; col<ncols; col++)  FreeSparseVector(Acol[col]);
+  free(Acol);
 
   return INFO;
 }
