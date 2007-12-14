a19b0c3d8123f01fcdb735e6e9f08a56ac478b0b
wolf <wolf>
2007-12-14 17:11:21 +0000

    LinSolve_withLAPACK.c LinSolve_withUMFPACK.c are no longer needed

--- src/physics/Poisson/Poisson.h
+++ src/physics/Poisson/Poisson.h
@@ -12,16 +12,3 @@ void J_Poisson(tVarList *vlJdu, tVarList *vldu,
                tVarList *vlduDerivs, tVarList *vlu);
 void Precon_I(tVarList *vlJdu, tVarList *vldu,
               tVarList *vlduDerivs, tVarList *vlu);
-
-
-int LinSolve_withLAPACK(tVarList *x, tVarList *b, 
-            tVarList *r, tVarList *c1,tVarList *c2,
-	    int itmax, double tol, double *normres,
-	    void (*lop)(tVarList *, tVarList *, tVarList *, tVarList *), 
-	    void (*precon)(tVarList *, tVarList *, tVarList *, tVarList *));
-
-int LinSolve_withUMFPACK(tVarList *x, tVarList *b, 
-            tVarList *r, tVarList *c1,tVarList *c2,
-	    int itmax, double tol, double *normres,
-	    void (*lop)(tVarList *, tVarList *, tVarList *, tVarList *), 
-	    void (*precon)(tVarList *, tVarList *, tVarList *, tVarList *));
