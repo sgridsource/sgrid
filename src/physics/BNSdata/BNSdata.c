4956b89bd637d3c4e84ddb2425917b00ad8d9eb9
wolf <wolf>
2007-12-21 17:42:04 +0000

    add templates_BICGSTAB and templates_CGS

--- src/physics/BNSdata/BNSdata.c
+++ src/physics/BNSdata/BNSdata.c
@@ -186,6 +186,10 @@ int BNSdata_solve(tGrid *grid)
     linear_solver=LAPACK_dgesv_wrapper;
   else if(Getv("BNSdata_linSolver", "templates_GMRES"))
     linear_solver=templates_gmres_wrapper;
+  else if(Getv("BNSdata_linSolver", "templates_BICGSTAB"))
+    linear_solver=templates_bicgstab_wrapper;
+  else if(Getv("BNSdata_linSolver", "templates_CGS"))
+    linear_solver=templates_cgs_wrapper;
   else if(Getv("BNSdata_linSolver", "UMFPACK"))
     linear_solver=UMFPACK_solve_forSortedVars_wrapper; // UMFPACK_solve_wrapper;
   else if(Getv("BNSdata_linSolver", "WTsolver"))
