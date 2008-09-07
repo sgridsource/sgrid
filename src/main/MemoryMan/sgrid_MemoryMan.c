c759f485e1cabea39e654db6b2f11a7a285351f0
wolf <wolf>
2008-09-07 20:34:51 +0000

    change some default par values so that ParManipulator can be used

--- src/main/MemoryMan/sgrid_MemoryMan.c
+++ src/main/MemoryMan/sgrid_MemoryMan.c
@@ -32,49 +32,49 @@ int sgrid_MemoryMan()
 
     /* number of points in all 3 directions */
     snprintf(str, 999, "box%d_n1", b);
-    AddPar(str, "1", "number of points in x-direction");
+    AddPar(str, "n1", "number of points in X-direction");
     snprintf(str, 999, "box%d_n2", b);
-    AddPar(str, "1", "number of points in y-direction");
+    AddPar(str, "n2", "number of points in Y-direction");
     snprintf(str, 999, "box%d_n3", b);
-    AddPar(str, "1", "number of points in z-direction");
+    AddPar(str, "n3", "number of points in Z-direction");
 
     /* min and max values in all 3 coords */
     snprintf(str, 999, "box%d_min1", b);
-    AddPar(str, "-1", "lower boundary in x-direction");
+    AddPar(str, "min1", "lower boundary in X-direction");
     snprintf(str, 999, "box%d_max1", b);
-    AddPar(str, "+1", "upper boundary in x-direction");
+    AddPar(str, "max1", "upper boundary in X-direction");
     
     snprintf(str, 999, "box%d_min2", b);
-    AddPar(str, "-1", "lower boundary in y-direction");
+    AddPar(str, "min2", "lower boundary in Y-direction");
     snprintf(str, 999, "box%d_max2", b);
-    AddPar(str, "+1", "upper boundary in y-direction");
+    AddPar(str, "max2", "upper boundary in Y-direction");
     
     snprintf(str, 999, "box%d_min3", b);
-    AddPar(str, "-1", "lower boundary in z-direction");
+    AddPar(str, "min3", "lower boundary in Z-direction");
     snprintf(str, 999, "box%d_max3", b);
-    AddPar(str, "+1", "upper boundary in z-direction");
+    AddPar(str, "max3", "upper boundary in Z-direction");
     
     /* basis functions used */
     snprintf(str, 999, "box%d_basis1", b);
-    AddPar(str, "ChebExtrema", 
-                "basis functions in x-direction [ChebExtrema, ChebZeros]");
+    AddPar(str, "basis1", 
+                "basis functions in X-direction [ChebExtrema, ChebZeros]");
     snprintf(str, 999, "box%d_basis2", b);
-    AddPar(str, "ChebExtrema", 
-                "basis functions in y-direction [ChebExtrema, ChebZeros]");
+    AddPar(str, "basis2", 
+                "basis functions in Y-direction [ChebExtrema, ChebZeros]");
     snprintf(str, 999, "box%d_basis3", b);
-    AddPar(str, "ChebExtrema", 
-                "basis functions in z-direction [ChebExtrema, ChebZeros]");
+    AddPar(str, "basis3", 
+                "basis functions in Z-direction [ChebExtrema, ChebZeros]");
 
     /* simple filter amounts used */
     snprintf(str, 999, "box%d_filter1", b);
     AddPar(str, "0", 
-                "filter amount in x-direction [some number up to n1]");
+                "filter amount in X-direction [some number up to n1]");
     snprintf(str, 999, "box%d_filter2", b);
     AddPar(str, "0", 
-                "filter amount in y-direction [some number up to n2]");
+                "filter amount in Y-direction [some number up to n2]");
     snprintf(str, 999, "box%d_filter3", b);
     AddPar(str, "0", 
-                "filter amount in z-direction [some number up to n3]");
+                "filter amount in Z-direction [some number up to n3]");
   }
   return 0;
 }
