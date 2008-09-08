f01a8637483dcf11a3c7bf199b8d96345be5ea67
wolf <wolf>
2008-09-08 02:26:21 +0000

    if we set e.g. box1_n1, set it to the right value and also to n1. E.g. if n1 = 12, set box1_n1 = 12 n1 so that box1_n1 was n1, it still is also n1.

--- src/utility/ParManipulator/ParManipulator.c
+++ src/utility/ParManipulator/ParManipulator.c
@@ -14,38 +14,87 @@ int ParManipulator_setPars(tGrid *grid)
   for(b=0; b<nboxes; b++)
   {
     char str[1000];
+    char val[1000];
 
     /* set the n1,n2,n3 of in each box */
     snprintf(str, 999, "box%d_n1", b);
-    if(Getv(str, "n1")) Sets(str, Gets("n1"));
+    if(Getv(str, "n1")) 
+    {
+      snprintf(val, 999, "%s n1", Gets("n1"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_n2", b);
-    if(Getv(str, "n2")) Sets(str, Gets("n2"));
+    if(Getv(str, "n2")) 
+    {
+      snprintf(val, 999, "%s n2", Gets("n2"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_n3", b);
-    if(Getv(str, "n3")) Sets(str, Gets("n3"));
+    if(Getv(str, "n3")) 
+    {
+      snprintf(val, 999, "%s n3", Gets("n3"));
+      Sets(str, val);
+    }
 
     /* set the min1,min2,min3 of in each box */
     snprintf(str, 999, "box%d_min1", b);
-    if(Getv(str, "min1")) Sets(str, Gets("min1"));
+    if(Getv(str, "min1")) 
+    {
+      snprintf(val, 999, "%s min1", Gets("min1"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_min2", b);
-    if(Getv(str, "min2")) Sets(str, Gets("min2"));
+    if(Getv(str, "min2")) 
+    {
+      snprintf(val, 999, "%s min2", Gets("min2"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_min3", b);
-    if(Getv(str, "min3")) Sets(str, Gets("min3"));
+    if(Getv(str, "min3")) 
+    {
+      snprintf(val, 999, "%s min3", Gets("min3"));
+      Sets(str, val);
+    }
 
     /* set the max1,max2,max3 of in each box */
     snprintf(str, 999, "box%d_max1", b);
-    if(Getv(str, "max1")) Sets(str, Gets("max1"));
+    if(Getv(str, "max1")) 
+    {
+      snprintf(val, 999, "%s max1", Gets("max1"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_max2", b);
-    if(Getv(str, "max2")) Sets(str, Gets("max2"));
+    if(Getv(str, "max2")) 
+    {
+      snprintf(val, 999, "%s max2", Gets("max2"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_max3", b);
-    if(Getv(str, "max3")) Sets(str, Gets("max3"));
+    if(Getv(str, "max3")) 
+    {
+      snprintf(val, 999, "%s max3", Gets("max3"));
+      Sets(str, val);
+    }
 
     /* set the basis1,basis2,basis3 of in each box */
     snprintf(str, 999, "box%d_basis1", b);
-    if(Getv(str, "basis1")) Sets(str, Gets("basis1"));
+    if(Getv(str, "basis1")) 
+    {
+      snprintf(val, 999, "%s basis1", Gets("basis1"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_basis2", b);
-    if(Getv(str, "basis2")) Sets(str, Gets("basis2"));
+    if(Getv(str, "basis2")) 
+    {
+      snprintf(val, 999, "%s basis2", Gets("basis2"));
+      Sets(str, val);
+    }
     snprintf(str, 999, "box%d_basis3", b);
-    if(Getv(str, "basis3")) Sets(str, Gets("basis3"));
+    if(Getv(str, "basis3")) 
+    {
+      snprintf(val, 999, "%s basis3", Gets("basis3"));
+      Sets(str, val);
+    }
 
     /* set dt */
     if(Getv("ParManipulator_dt_scaling", "1/n1^2"))
