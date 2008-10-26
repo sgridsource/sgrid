bf78b21758958a7861e62288d335ef9c8ace7320
wolf <wolf>
2008-10-26 20:47:33 +0000

    optimization

--- src/utility/Coordinates/cartesianDerivs.c
+++ src/utility/Coordinates/cartesianDerivs.c
@@ -175,7 +175,7 @@ void cart_partials(tBox *box, double *u, double *u1, double *u2, double *u3)
 
   if( box->x_of_X[1] != NULL )
   {
-    int i,m, ind;
+    int i,m,m_1, ind;
     double *Xp = box->v[Ind("X")];
     double *Yp = box->v[Ind("Y")];
     double *Zp = box->v[Ind("Z")];
@@ -185,24 +185,44 @@ void cart_partials(tBox *box, double *u, double *u1, double *u2, double *u3)
     du[1] = u1;        du[2] = u2;        du[3] = u3;
 
     /* compute cartesian derivs at all points */
-    forallpoints(box,ind)
-    {
-      double X = Xp[ind];
-      double Y = Yp[ind];
-      double Z = Zp[ind];
-    
-      /* Transform derivs to cartesian coords */
-      for(m=1; m<=3; m++)
+    if(box->v[dXd]) /* use stored coord trafos */
+      forallpoints(box,ind)
       {
-        dv[m] = 0.0;
-          
-        for(i=1; i<=3; i++)
-          dv[m] += dX_dx(i,m, dXd, box, ind, X,Y,Z) * du[i][ind];
+        /* Transform derivs to cartesian coords */
+        for(m=1; m<=3; m++)
+        {
+          dv[m] = 0.0;
+          m_1 = m-1;
+            
+          for(i=1; i<=3; i++)
+            /* dv[m] += dX_dx(i,m, dXd, box, ind, X,Y,Z) * du[i][ind]; */
+            dv[m] += box->v[dXd + 3*(i-1) + m_1][ind] * du[i][ind];
+        }
+        /* copy dv into du */
+        for(m=1; m<=3; m++)
+          du[m][ind] = dv[m];
       }
-      /* copy dv into du */
-      for(m=1; m<=3; m++)
-        du[m][ind] = dv[m];
-    } /* end loop over all points */
+    else  /* compute  coord trafos */
+      forallpoints(box,ind)
+      {
+        double X = Xp[ind];
+        double Y = Yp[ind];
+        double Z = Zp[ind];
+      
+        /* Transform derivs to cartesian coords */
+        for(m=1; m<=3; m++)
+        {
+          dv[m] = 0.0;
+          m_1 = m-1;
+            
+          for(i=1; i<=3; i++)
+            /* dv[m] += dX_dx(i,m, dXd, box, ind, X,Y,Z) * du[i][ind]; */
+            dv[m] += box->dX_dx[i][m]((void *) box, ind, X,Y,Z) * du[i][ind];
+        }
+        /* copy dv into du */
+        for(m=1; m<=3; m++)
+          du[m][ind] = dv[m];
+      } /* end cartesian derivs at all points */
 
     /* set derivs at singular points */
     for(m=1; m<=3; m++)
@@ -232,7 +252,7 @@ void cart_partial_all(tBox *box, double *u, double *u1, double *u2, double *u3,
   /* Note: the pointers x_of_X[i] are NULL if X_l = x_l */
   if( box->x_of_X[1] != NULL )
   {
-    int i,j,m,n, ind;
+    int i,j,m,n,m_1,n_1, vi, ind;
     double *Xp = box->v[Ind("X")];
     double *Yp = box->v[Ind("Y")];
     double *Zp = box->v[Ind("Z")];
@@ -249,50 +269,104 @@ void cart_partial_all(tBox *box, double *u, double *u1, double *u2, double *u3,
     if( box->ddX_dxdx[1][1][1] == NULL )
       errorexit("cart_partial_all: box->ddX_dxdx[i][m][n] = NULL.");
 
-    /* loop over all points */
-    forallpoints(box,ind)
-    {
-      double X = Xp[ind];
-      double Y = Yp[ind];
-      double Z = Zp[ind];
-      
-      /* Transform derivs to cartesian coords */
-      /* Note:
-      d u / dx^m = (dX^i / dx^m)  (d u / dX^i)
-      d^2 u / dx^m dx^n =  (dX^i / dx^m dx^n) (d u / dX^i)
-                          + (dX^i / dx^m) (d u / dX^i dx^n)
-                        =   (dX^i / dx^m dx^n) (d u / dX^i)
-                          + (dX^i / dx^m) (dX^j / dx^n) (d u / dX^i dX^j)  */
-      for(m=1; m<=3; m++)
+    /* compute cartesian derivs at all points */
+    if(box->v[ddXdd] && box->v[dXd]) /* use stored coord trafos */
+      forallpoints(box,ind) /* loop over all points */
       {
-        dv[m] = 0.0;
-        
-        for(i=1; i<=3; i++)
-          dv[m] += dX_dx(i,m, dXd, box, ind, X,Y,Z) * du[i][ind];
-
-        for(n=m; n<=3; n++)
+        /* Transform derivs to cartesian coords */
+        /* Note:
+        d u / dx^m = (dX^i / dx^m)  (d u / dX^i)
+        d^2 u / dx^m dx^n =  (dX^i / dx^m dx^n) (d u / dX^i)
+                            + (dX^i / dx^m) (d u / dX^i dx^n)
+                          =   (dX^i / dx^m dx^n) (d u / dX^i)
+                            + (dX^i / dx^m) (dX^j / dx^n) (d u / dX^i dX^j)  */
+        for(m=1; m<=3; m++)
         {
-          ddv[m][n] = 0.0;
+          dv[m] = 0.0;
+          m_1 = m-1;
 
           for(i=1; i<=3; i++)
-            ddv[m][n] += ddX_dxdx(i,m,n, ddXdd, box, ind, X,Y,Z) * du[i][ind];
-
-          for(i=1; i<=3; i++)
-            for(j=1; j<=3; j++)
-              ddv[m][n] += dX_dx(i,m, dXd, box, ind, X,Y,Z) *
-                           dX_dx(j,n, dXd, box, ind, X,Y,Z) *
-                           ddu[i][j][ind];
+            dv[m] += box->v[dXd + 3*(i-1) + m_1][ind] * du[i][ind];
+  
+          for(n=m; n<=3; n++)
+          {
+            ddv[m][n] = 0.0;
+            n_1 = n-1;
+  
+            for(i=1; i<=3; i++)
+            {
+              if(m==1)  vi=ddXdd +6*(i-1) +(n_1);
+              else      vi=ddXdd +6*(i-1) +m+n_1;
+              /* ddv[m][n] += ddX_dxdx(i,m,n, ddXdd, box, ind, X,Y,Z) * du[i][ind]; */
+              ddv[m][n] += box->v[vi][ind] * du[i][ind];
+            }
+            for(i=1; i<=3; i++)
+              for(j=1; j<=3; j++)
+                /* ddv[m][n] += dX_dx(i,m, dXd, box, ind, X,Y,Z) *
+                             dX_dx(j,n, dXd, box, ind, X,Y,Z) *
+                             ddu[i][j][ind]; */
+                ddv[m][n] += box->v[dXd + 3*(i-1) + m_1][ind] * 
+                             box->v[dXd + 3*(j-1) + n_1][ind] *
+                             ddu[i][j][ind];
+          }
         }
-      }
-
-      /* copy dv and ddv into du and ddu */
-      for(m=1; m<=3; m++)
+        /* copy dv and ddv into du and ddu */
+        for(m=1; m<=3; m++)
+        {
+          du[m][ind] = dv[m];
+          
+          for(n=m; n<=3; n++)
+            ddu[m][n][ind] = ddv[m][n];
+        }
+      } /* end loop over all points */
+    else /* compute coord trafos */
+      forallpoints(box,ind) /* loop over all points */
       {
-        du[m][ind] = dv[m];
+        double X = Xp[ind];
+        double Y = Yp[ind];
+        double Z = Zp[ind];
         
-        for(n=m; n<=3; n++)
-          ddu[m][n][ind] = ddv[m][n];
-      }
-    } /* end loop over all points */
+        /* Transform derivs to cartesian coords */
+        /* Note:
+        d u / dx^m = (dX^i / dx^m)  (d u / dX^i)
+        d^2 u / dx^m dx^n =  (dX^i / dx^m dx^n) (d u / dX^i)
+                            + (dX^i / dx^m) (d u / dX^i dx^n)
+                          =   (dX^i / dx^m dx^n) (d u / dX^i)
+                            + (dX^i / dx^m) (dX^j / dx^n) (d u / dX^i dX^j)  */
+        for(m=1; m<=3; m++)
+        {
+          dv[m] = 0.0;
+
+          for(i=1; i<=3; i++)
+            dv[m] += box->dX_dx[i][m]((void *) box, ind, X,Y,Z) * du[i][ind];
+              
+          for(n=m; n<=3; n++)
+          {
+            ddv[m][n] = 0.0;
+  
+            for(i=1; i<=3; i++)
+              /* ddv[m][n] += ddX_dxdx(i,m,n, ddXdd, box, ind, X,Y,Z) * du[i][ind]; */
+              ddv[m][n] += box->ddX_dxdx[i][m][n]((void *) box, ind, X,Y,Z) *
+                           du[i][ind];
+
+            for(i=1; i<=3; i++)
+              for(j=1; j<=3; j++)
+                /* ddv[m][n] += dX_dx(i,m, dXd, box, ind, X,Y,Z) *
+                             dX_dx(j,n, dXd, box, ind, X,Y,Z) *
+                             ddu[i][j][ind]; */
+                ddv[m][n] += box->dX_dx[i][m]((void *) box, ind, X,Y,Z) *
+                             box->dX_dx[j][n]((void *) box, ind, X,Y,Z) *
+                             ddu[i][j][ind];
+          }
+        }
+        /* copy dv and ddv into du and ddu */
+        for(m=1; m<=3; m++)
+        {
+          du[m][ind] = dv[m];
+          
+          for(n=m; n<=3; n++)
+            ddu[m][n][ind] = ddv[m][n];
+        }
+      } /* end compute cartesian derivs at all points */
   } /* end if */
 }
