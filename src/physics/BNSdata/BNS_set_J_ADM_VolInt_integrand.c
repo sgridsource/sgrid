fddf063ce4fc3b6026638589abca32fddda91647
wolf <wolf>
2008-11-04 15:14:55 +0000

    add volint integrand for J_ADM

---
+++ src/physics/BNSdata/BNS_set_J_ADM_VolInt_integrand.c
@@ -0,0 +1,243 @@
+/* BNS_set_J_ADM_VolInt_integrand.c */
+/* Copyright (C) 2005 Wolfgang Tichy & Bernd Bruegmann, 4.11.2008 */
+/* Produced with Mathematica */
+
+#include "sgrid.h"
+#include "BNSdata.h"
+
+#define Power(x,y) (pow((double) (x), (double) (y)))
+#define Log(x)     log((double) (x)))
+#define pow2(x)    ((x)*(x))
+#define pow2inv(x) (1.0/((x)*(x)))
+#define Cal(x,y,z) ((x)?(y):(z))
+
+
+
+
+void BNS_set_J_ADM_VolInt_integrand(tGrid *grid, int iInteg)
+{
+double n = Getd("BNSdata_n");
+double kappa = Getd("BNSdata_kappa");
+double Omega = Getd("BNSdata_Omega");
+double xCM = Getd("BNSdata_x_CM");
+
+int bi;
+
+forallboxes(grid,bi)
+{
+tBox *box = grid->box[bi];
+int ijk;
+
+
+
+int index_x = Ind("x");
+double *x = box->v[index_x + 0];
+int index_y = Ind("y");
+double *y = box->v[index_y + 0];
+int index_BNSdata_Psi = Ind("BNSdata_Psi");
+double *Psi = box->v[index_BNSdata_Psi + 0];
+int index_BNSdata_Bx = Ind("BNSdata_Bx");
+double *B1 = box->v[index_BNSdata_Bx + 0];
+double *B2 = box->v[index_BNSdata_Bx + 1];
+double *B3 = box->v[index_BNSdata_Bx + 2];
+int index_BNSdata_alphaP = Ind("BNSdata_alphaP");
+double *alphaP = box->v[index_BNSdata_alphaP + 0];
+int index_BNSdata_Sigma = Ind("BNSdata_Sigma");
+double *Sigma = box->v[index_BNSdata_Sigma + 0];
+int index_BNSdata_q = Ind("BNSdata_q");
+double *q = box->v[index_BNSdata_q + 0];
+int index_BNSdata_vRSx = Ind("BNSdata_vRSx");
+double *vRS1 = box->v[index_BNSdata_vRSx + 0];
+double *vRS2 = box->v[index_BNSdata_vRSx + 1];
+double *vRS3 = box->v[index_BNSdata_vRSx + 2];
+int index_BNSdata_Sigmax = Ind("BNSdata_Sigmax");
+double *dSigma1 = box->v[index_BNSdata_Sigmax + 0];
+double *dSigma2 = box->v[index_BNSdata_Sigmax + 1];
+double *dSigma3 = box->v[index_BNSdata_Sigmax + 2];
+double *Integ = box->v[iInteg+0];
+
+
+double alpha;
+double alpha2;
+double beta1;
+double beta2;
+double beta3;
+double jup1;
+double jup2;
+double jup3;
+double OmegaCrossR1;
+double OmegaCrossR2;
+double OmegaCrossR3;
+double oouzerosqr;
+double P;
+double Psi2;
+double Psi4;
+double rho0;
+double rhoE;
+double uzerosqr;
+double vR1;
+double vR2;
+double vR3;
+double vRI1;
+double vRI2;
+double vRI3;
+
+
+
+/* Jetzt geht's los! */
+
+FirstDerivsOf_S(box, Ind("BNSdata_Sigma"),     Ind("BNSdata_Sigmax")); 
+
+
+forallpoints(box, ijk) { 
+
+alpha
+=
+alphaP[ijk]/Psi[ijk]
+;
+
+alpha2
+=
+pow2(alpha)
+;
+
+Psi2
+=
+pow2(Psi[ijk])
+;
+
+Psi4
+=
+pow2(Psi2)
+;
+
+OmegaCrossR1
+=
+-(Omega*y[ijk])
+;
+
+OmegaCrossR2
+=
+Omega*(-xCM + x[ijk])
+;
+
+OmegaCrossR3
+=
+0
+;
+
+beta1
+=
+OmegaCrossR1 + B1[ijk]
+;
+
+beta2
+=
+OmegaCrossR2 + B2[ijk]
+;
+
+beta3
+=
+OmegaCrossR3 + B3[ijk]
+;
+
+vRI1
+=
+dSigma1[ijk]
+;
+
+vRI2
+=
+dSigma2[ijk]
+;
+
+vRI3
+=
+dSigma3[ijk]
+;
+
+vR1
+=
+vRI1 + vRS1[ijk]
+;
+
+vR2
+=
+vRI2 + vRS2[ijk]
+;
+
+vR3
+=
+vRI3 + vRS3[ijk]
+;
+
+oouzerosqr
+=
+alpha2 - Psi4*(2.*(beta1*vR1 + beta2*vR2 + beta3*vR3) + pow2(beta1) + 
+     pow2(beta2) + pow2(beta3) + pow2(vR1) + pow2(vR2) + pow2(vR3))
+;
+
+uzerosqr
+=
+1./oouzerosqr
+;
+
+rho0
+=
+Power(q[ijk]/kappa,n)
+;
+
+P
+=
+rho0*q[ijk]
+;
+
+rhoE
+=
+rho0 + n*rho0*q[ijk]
+;
+
+
+
+/* conditional */
+if (q[ijk] == 0) {
+
+uzerosqr
+=
+0
+;
+
+}
+/* if (q[ijk] == 0) */
+
+
+jup1
+=
+alpha*(P + rhoE)*uzerosqr*(beta1 + vR1)
+;
+
+jup2
+=
+alpha*(P + rhoE)*uzerosqr*(beta2 + vR2)
+;
+
+jup3
+=
+alpha*(P + rhoE)*uzerosqr*(beta3 + vR3)
+;
+
+Integ[ijk]
+=
+Psi4*(jup2*(-xCM + x[ijk]) - jup1*y[ijk])
+;
+
+
+} /* end of points loop */ 
+
+} /* end of boxes */
+
+
+}  /* end of function */
+
+/* BNS_set_J_ADM_VolInt_integrand.c */
+/* nvars = 16, n* = 60,  n/ = 22,  n+ = 61, n = 143, O = 1 */
---
+++ src/physics/BNSdata/BNS_set_J_ADM_VolInt_integrand.m
@@ -0,0 +1,161 @@
+(* BNS_set_J_ADM_VolInt_integrand.m 
+   Wolfgang Tichy  11/2008       *)
+
+(* set integrand for inner volume integrals of J_ADM *)
+
+
+(* variables *)
+variables = {Psi, B[a], alphaP, Sigma, dSigma[a], q, vRS[a], x, y, Integ}
+
+
+constvariables = {OmegaCrossR[a]}
+
+(* compute in this order *)
+tocompute = {
+
+  Cinstruction == "FirstDerivsOf_S(box, Ind(\"BNSdata_Sigma\"),
+					Ind(\"BNSdata_Sigmax\"));",
+
+  (* loop of all points *)
+  Cinstruction == "forallpoints(box, ijk) {",
+
+  (* set lapse and Psi4 *)
+  alpha  == alphaP/Psi,
+  alpha2 == alpha*alpha,
+  Psi2   == Psi*Psi,
+  Psi4   == Psi2*Psi2,
+
+  (* Omega \times r term *)
+  OmegaCrossR1 == - Omega y,
+  OmegaCrossR2 == + Omega (x-xCM),
+  OmegaCrossR3 == 0,
+
+  (* shift in rotating frame *)
+  beta[a] == B[a] + OmegaCrossR[a],
+
+  (* irrotational part of 3-vel in rotating frame*)
+  vRI[a] == dSigma[a],
+
+  (* vR[a] is 3-vel. in rotating frame *)
+  vR[a] == vRS[a] + vRI[a],
+
+  (* compute square of u^0 in rotating frame *)
+  oouzerosqr == alpha2 - Psi4 delta[b,c] (beta[b] + vR[b]) (beta[c] + vR[c]),
+  uzerosqr == 1.0/oouzerosqr,
+
+  (* rest mass density, pressure, and total energy density *)
+  rho0 == Power[q/kappa, n],
+  P    == q rho0,
+  rhoE == rho0 (1 + n q),
+
+  (* if q=0 all matter vars are zero, which can be enforced by uzerosqr=0 *)
+  Cif == (q==0),
+    uzerosqr == 0,
+  Cif == end,
+
+  (* get fluid var j in 3+1 *)
+  jup[a] == alpha (rhoE + P) uzerosqr (vR[a]+beta[a]),
+
+ (* Integrand for rest mass without all 3-volume element
+     factors such as Psi^6 r^2 sin(theta) *)
+  Integ == Psi4 ( (x-xCM)*jup2 - y*jup1 ),
+
+  Cinstruction == "} /* end of points loop */\n"
+}
+
+
+(* symmetries *)
+ginv[a_,b_] := ginv[b,a] /; !OrderedQ[{a,b}]
+
+(************************************************************************)
+(* information for C output *)
+
+(* information about the function 
+   the C file will be in Cfunctionfile 
+*)
+
+CFunctionFile = "BNS_set_J_ADM_VolInt_integrand.c"
+
+(* the head of the function *)
+BeginCFunction[] := Module[{},
+
+  pr["#include \"sgrid.h\"\n"];
+  pr["#include \"BNSdata.h\"\n"];
+  pr["\n"];
+  pr["#define Power(x,y) (pow((double) (x), (double) (y)))\n"];
+  pr["#define Log(x)     log((double) (x)))\n"];
+  pr["#define pow2(x)    ((x)*(x))\n"];
+  pr["#define pow2inv(x) (1.0/((x)*(x)))\n"];
+  pr["#define Cal(x,y,z) ((x)?(y):(z))\n\n"];
+
+  pr["\n\n\n"];
+
+  pr["void BNS_set_J_ADM_VolInt_integrand(tGrid *grid, int iInteg)\n"];
+  pr["{\n"];
+
+  pr["double n = Getd(\"BNSdata_n\");\n"];
+  pr["double kappa = Getd(\"BNSdata_kappa\");\n"];
+  pr["double Omega = Getd(\"BNSdata_Omega\");\n"];
+  pr["double xCM = Getd(\"BNSdata_x_CM\");\n"];
+  pr["\n"];
+
+  pr["int bi;\n"];
+  pr["\n"];
+
+  pr["forallboxes(grid,bi)\n"];
+  pr["{\n"];
+  pr["tBox *box = grid->box[bi];\n"];
+  pr["int ijk;\n\n"];
+  pr["\n"];
+  pr["\n"];
+];
+
+(* custom variable declaration
+   we have to translate between the tensor names and the C variables  
+   we may need special variables, for example for parameters
+*)
+variabledeclarations[] := Module[{},
+
+  prdecvarname[{x},      "x"];
+  prdecvarname[{y},      "y"];
+
+  prdecvarname[{Psi},     "BNSdata_Psi"];
+  prdecvarname[{B[a]},    "BNSdata_Bx"];
+  prdecvarname[{alphaP},  "BNSdata_alphaP"];
+  prdecvarname[{Sigma},   "BNSdata_Sigma"];
+  prdecvarname[{q},      "BNSdata_q"];
+  prdecvarname[{vRS[a]}, "BNSdata_vRSx"];
+  prdecvarname[{dSigma[a]},     "BNSdata_Sigmax"];
+
+  prdecvar[{Integ}, "iInteg"];
+
+  pr["\n"];
+];    
+(* auxillary variables are automatically inserted here *)
+
+(* custom C-code which is placed directly after the variable declarations *)
+InitializationCommands[] := Module[{},
+
+  pr["/* Jetzt geht's los! */\n"];
+
+];
+
+(* the end or tail of the function (we need at least a }) *)
+EndCFunction[] := Module[{},
+
+  pr["} /* end of boxes */\n"];
+  pr["\n\n"];
+  pr["}  /* end of function */\n\n"];
+];
+
+
+(* to turn off optimization for development set optimizeflag = False *)
+optimizeflag = True;
+
+(* use 3d tensors the default is 3 *)
+TensorEquationsDim = 3;
+
+(************************************************************************)
+(* now we are ready to go *)
+
+<< "../../Math/MathToC/TensorEquationsToC.m"
