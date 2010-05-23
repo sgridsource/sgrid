02ef4189b15e173eab9bb27983080f7c58831ead
wolf <wolf>
2010-05-23 01:49:55 +0000

    changing simpson rule: using 3/8 rule for first 4 points can cause oscillations in integral, when total number of points switches from odd to even. So we use last 4 points for 3/8 rule

--- src/utility/NumericUtils/integrals_1Dgrid.c
+++ src/utility/NumericUtils/integrals_1Dgrid.c
@@ -7,6 +7,32 @@
 double integrate_simpson_1Dgrid(double* f, double dx, int i1, int i2)
 {
   double sum;
+  int    i, ioff;
+
+  sum  = 0;
+  ioff = 0;
+
+  /* If total number of points is even, use 3/8 rule for last 4 points */
+  if((i2-i1+1) % 2 == 0)
+    ioff = 3;
+
+  /* simpson rule for odd numer of points */
+  for(i = i1; i < i2-ioff-1; i += 2)
+    sum += dx / 3 * (f[i] + 4 * f[i+1] + f[i+2]);
+
+  /* If total number of points is even, use 3/8 rule for last 4 points */
+  if((i2-i1+1) % 2 == 0)
+    sum += 3 * dx * (f[i2-3] + 3 * (f[i2-2] + f[i2-1]) + f[i2]) / 8;
+
+  return sum;
+}
+
+/* old integration with Simpson rule:
+   using 3/8 rule for first 4 points can cause oscillations in integral,
+   when total number of points switches from odd to even... */
+double integrate_simpson_old_1Dgrid(double* f, double dx, int i1, int i2)
+{
+  double sum;
   int i, ioff;
 
 
