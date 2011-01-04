81fc7fc488f5036d07e62d57d71ca5784ea2c61e
wolf <wolf>
2011-01-04 01:53:29 +0000

    make find_ind_closest_to_Z0 available globally

--- src/utility/output/output.h
+++ src/utility/output/output.h
@@ -5,8 +5,5 @@ void gnuplot_out2d_boxvar(tBox *box, char *name);
 void xgraph_out1_boxvar(tBox *box, char *name);
 void dump3d_boxvar(tBox *box, char *name);
 
-int find_ind_closest_to_X0(tBox *box, double X0);
-int find_ind_closest_to_Y0(tBox *box, double Y0);
-int find_ind_closest_to_Z0(tBox *box, double Z0);
 void output0d_value(char *filename, double time, double val);
 void output0d_boxvar(tBox *box, char *name);
--- src/utility/output/sgrid_output.h
+++ src/utility/output/sgrid_output.h
@@ -2,13 +2,13 @@
 /* Wolfgang Tichy, April 2005 */
 
     
-/* this value marks points in the output for which no data is available */
-
-
 /* output.c */
 int timeforoutput_di_dt(tGrid *grid, int di, double dt);
 int timeforoutput_index(tGrid *grid, int index);
 int timeforoutput(tGrid *grid, tVarList *vl);
 int timeforoutput_any(tGrid *grid);
+int find_ind_closest_to_X0(tBox *box, double X0);
+int find_ind_closest_to_Y0(tBox *box, double Y0);
+int find_ind_closest_to_Z0(tBox *box, double Z0);
 
 int write_grid(tGrid *grid);
