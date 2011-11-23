6c13614af55f7ebc9236889dc6d109b138fda577
wolf <wolf>
2011-11-23 13:43:50 +0000

    always add pars for NumberChecker

--- src/utility/NumberChecker/sgrid_NumberChecker.c
+++ src/utility/NumberChecker/sgrid_NumberChecker.c
@@ -8,20 +8,22 @@
 
 int sgrid_NumberChecker() 
 {
-  if (!Getv("physics", "NumberChecker")) return 0;
   printf("Adding NumberChecker\n");
 
+  /* pars to check for NANs */
+  AddPar("NumberChecker_exitifNAN", "no", 
+	 "exit if NAN is found [varname,no] (not a list)");
+  AddPar("NumberChecker_INF", "1e300", 
+	 "consider a number INF/NAN if it's above this [any large number]");
+
+  /* add functions only if NumberChecker is on */
+  if (!Getv("physics", "NumberChecker")) return 0;
   /* functions */
   // AddFun(ANALYZE, computeADMconstraints, "compute ADM constraints");
 
   /* 3+1 field variables */
   // AddVar("g",      "(ij)", "metric");
 
-  /* check for NANs */
-  AddPar("NumberChecker_exitifNAN", "no", 
-	 "exit if NAN is found [varname,no] (not a list)");
-  AddPar("NumberChecker_INF", "1e300", 
-	 "consider a number INF/NAN if it's above this [any large number]");
   if (!Getv("NumberChecker_exitifNAN", "no")) 
     AddFun(POST_EVOLVE, NumberChecker_ExitIfNAN, "exit if NAN");
 
