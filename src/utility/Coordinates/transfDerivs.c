827bef739725252eb16c89abbd784d94720e35e5
wolf <wolf>
2013-03-21 22:17:43 +0000

    add dxdX_from_dxdU_dUdX and ddxdXdX_from_dxdU_dUdX_ddxdUdU_ddUdXdX to get dx^i/dX^m and d^2 x^i/(dX^m dX^n) from dx^i/dU^n , dU^n/dX^m and d^2 x^i/(dU^j dU^k) , d^2 U^j/(dX^m dX^n) (this is untested)

--- src/utility/Coordinates/transfDerivs.c
+++ src/utility/Coordinates/transfDerivs.c
@@ -4,6 +4,8 @@
 #include "sgrid.h"
 #include "Coordinates.h"
 
+
+
 /* compute dX^i/dx^n from dx^n/dX^l and
    d^2 X^i /(dx^j dx^k) from d^2 x^n/(dX^m dX^l) */
 /* IDEA:
@@ -14,7 +16,6 @@ d/dx^j ((dX^i/dx^n)(dx^n/dX^l)) = 0
 [d^2 X^i /(dx^j dx^n)](dx^n/dX^l) + (dX^i/dx^n) (dX^m/dx^j)d/dX^m (dx^n/dX^l) = 0  | *dX^l/dx^k
 d^2 X^i /(dx^j dx^k) + (dX^i/dx^n)(dX^m/dx^j)(dX^l/dx^k) d^2 x^n/(dX^m dX^l)=0
 */
-
 
 /* compute dX^i/dx^n from dx^n/dX^l */
 void dXdx_from_dxdX(double dXdx[4][4], double dxdX[4][4])
@@ -33,6 +34,7 @@ void dXdx_from_dxdX(double dXdx[4][4], double dxdX[4][4])
                 dxdX[1][1]*dxdX[2][3]*dxdX[3][2] - 
                 dxdX[1][2]*dxdX[2][1]*dxdX[3][3];
   LDOUBLE sum;
+  if(DetM==0.0) DetM=dequaleps*dequaleps*dequaleps;
   sum        = (-(dxdX[2][3]*dxdX[3][2]) + dxdX[2][2]*dxdX[3][3])/DetM;
   dXdx[1][1] = sum;
   sum        = (  dxdX[1][3]*dxdX[3][2]  - dxdX[1][2]*dxdX[3][3])/DetM;
@@ -73,5 +75,64 @@ void ddXdxdx_from_dXdx_ddxdXdX(double ddXdxdx[4][4][4],
       ddXdxdx_ijk -= dXdx[i][n]*dXdx[m][j]*dXdx[l][k]*ddxdXdX_nml;
     }
     ddXdxdx[i][j][k] = ddXdxdx_ijk;
+  }
+}
+
+
+/* 2 coord trafos: X -> U = g(X)  and  U -> x = f(U):
+    x^i = f^i(U^m)
+    U^j = g^j(X^n)
+compute dx^i/dX^m and d^2 x^i/(dX^m dX^n) from 
+   dx^i/dU^n , dU^n/dX^m and d^2 x^i/(dU^j dU^k) , d^2 U^j/(dX^m dX^n) */
+/* IDEA:
+dx^i/dX^m = dx^i/dU^n dU^n/dX^m
+d^2 x^i/(dX^m dX^n) = d/dX^m (dU^k/dX^n dx^i/dU^k)
+ = d^2 dU^k/(dX^m dX^n) dx^i/dU^k + dU^k/dX^n d^2 x^i/(dU^j dU^k) dU^j/dX^m
+ = (dx^i/dU^j) d^2 dU^j/(dX^m dX^n) + dU^j/dX^m dU^k/dX^n d^2 x^i/(dU^j dU^k)
+*/
+
+/* dx^i/dX^m = dx^i/dU^n dU^n/dX^m */
+void dxdX_from_dxdU_dUdX(double dxdX[4][4],
+                         double dxdU[4][4], double dUdX[4][4])
+{
+  int i,m,n;
+
+  for(i=1;i<=3;i++)
+  for(m=1;m<=3;m++)
+  {
+    LDOUBLE sum=0.0;
+
+    for(n=1;n<=3;n++) sum += dxdU[i][n] * dUdX[n][m];
+    dxdX[i][m] = sum;
+  }
+}
+
+/* d^2 x^i/(dX^m dX^n) = 
+  (dx^i/dU^j) d^2 dU^j/(dX^m dX^n) + dU^j/dX^m dU^k/dX^n d^2 x^i/(dU^j dU^k) */
+void ddxdXdX_from_dxdU_dUdX_ddxdUdU_ddUdXdX(double ddxdXdX[4][4][4],
+                         double dxdU[4][4], double dUdX[4][4],
+                         double ddxdUdU[4][4][4], double ddUdXdX[4][4][4])
+{
+  int i,m,n, j,k;
+
+  for(i=1;i<=3;i++)
+  for(m=1;m<=3;m++)
+  for(n=m;n<=3;n++)
+  {
+    LDOUBLE sum1=0.0;
+    LDOUBLE sum2=0.0;
+
+    for(j=1;j<=3;j++)
+    {
+      sum1 += dxdU[i][j] * ddUdXdX[j][m][n];
+      for(k=1;k<=3;k++)
+      {
+        LDOUBLE ddxdUdU_ijk;
+        if(k>=j)  ddxdUdU_ijk = ddxdUdU[i][j][k];
+        else      ddxdUdU_ijk = ddxdUdU[i][k][j];
+        sum2 += dUdX[j][m] * dUdX[k][n] * ddxdUdU_ijk;
+      }
+    }
+    ddxdXdX[i][m][n] = sum1 + sum2;
   }
 }
