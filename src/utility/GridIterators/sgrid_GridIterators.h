2cc8d22c00c6cd8e268a05576b4a54b49211f9f2
wolf <wolf>
2013-05-11 01:17:06 +0000

    add def

--- src/utility/GridIterators/sgrid_GridIterators.h
+++ src/utility/GridIterators/sgrid_GridIterators.h
@@ -56,6 +56,19 @@ int Newton(
 
 void Preconditioner_I(tVarList *vlJdu, tVarList *vldu,
                       tVarList *vlduDerivs, tVarList *vlu);
+int Newton_linesrch(
+  void  (*Fu)(tVarList *vl_Fu,  tVarList *vl_u,  tVarList *vl_c1, tVarList *vl_c2),
+  void (*Jdu)(tVarList *vl_Jdu, tVarList *vl_du, tVarList *vl_d1, tVarList *vl_d2),
+  tVarList *vlu, tVarList *vlFu, tVarList *vlc1, tVarList *vlc2,
+  int itmax, double tol, double *normres, int pr,
+  int (*linSolver)(tVarList *vl_du, tVarList *vl_Fu, 
+                   tVarList *vl_res, tVarList *vl_d1, tVarList *vl_d2,
+                   int lin_itmax, double lin_tol, double *lin_normres,
+	           void (*J_du)(tVarList *, tVarList *, tVarList *, tVarList *), 
+	           void (*lin_precon)(tVarList *, tVarList *, tVarList *, tVarList *)),
+  void (*Precon)(tVarList *Hinv_v, tVarList *v, tVarList *, tVarList *),
+  tVarList *vldu, tVarList *vlres, tVarList *vld1, tVarList *vld2,
+  int linSolv_itmax, double linSolv_tolFac, double linSolv_tol);
 
 /* wrappers for funcs from templates */
 int templates_gmres_wrapper(
