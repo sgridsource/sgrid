5b24570dfb98fac66047e7ed0d1f3678d189a487
Wolfgang <wolf@fau.edu>
2014-07-11 17:10:54 -0400

    change fd_jacobianP such that df[i][j] does not have columns full of zeros

--- src/utility/NumericUtils/fd_jacobian.c
+++ src/utility/NumericUtils/fd_jacobian.c
@@ -31,23 +31,31 @@ void fd_jacobian(int n, double x[], double fvec[], double **df,
 void fd_jacobianP(int n, double x[], double fvec[], double **df,
 	void (*vecfunc)(int, double [], double [], void *par), void *par)
 {
-	int i,j;
+	int i,j, k, df_nonzero;
 	double h,temp,*f;
 
 	f=vector(1,n);
-	for (j=1;j<=n;j++) {
+	for (j=1;j<=n;j++)
+	{
 		temp=x[j];
 		h=EPS*fabs(temp);
-		if (h < HMIN) h=HMIN; /* make sure h is never below HMIN */
-		x[j]=temp+h;
-		h=x[j]-temp;
-		(*vecfunc)(n,x,f, par);
-		x[j]=temp;
-		for (i=1;i<=n;i++) df[i][j]=(f[i]-fvec[i])/h;
+		if(h < HMIN) h=HMIN; /* make sure h is never below HMIN */
+		for(k=0; k<8; k++, h=h*10) /* increase h at most by 8 orders of mag. */
+		{
+			x[j]=temp+h;
+			h=x[j]-temp;
+			(*vecfunc)(n,x,f, par);
+			x[j]=temp;
+			for(df_nonzero=0, i=1; i<=n; i++)
+			{
+				df[i][j]=(f[i]-fvec[i])/h;
+				if(df[i][j] != 0.0) df_nonzero=1;
+			}
+			if(df_nonzero) break;
+		}
 	}
 	free_vector(f,1,n);
 }
-
 
 #undef HMIN
 #undef EPS
