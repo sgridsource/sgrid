7370e6f2e904f4192dd5f3db4935e8ed44ff720f
Wolfgang <wolf@fau.edu>
2015-08-02 19:33:43 +0200

    replace exit with abort in nrerror, so that we can use debugger

--- src/utility/NumericUtils/nrutil.c
+++ src/utility/NumericUtils/nrutil.c
@@ -19,7 +19,7 @@ void nrerror(char error_text[])
 	fprintf(stderr,"Numerical Recipes run-time error...\n");
 	fprintf(stderr,"%s\n",error_text);
 	fprintf(stderr,"...now exiting to system...\n");
-	exit(1);
+	abort();
 }
 
 double *vector(long nl, long nh)
