cc927e50ae022600c1df7188ccf029b4194608b9
Wolfgang <wolf@fau.edu>
2017-05-07 20:23:45 -0400

    add functions of the sort dy_dB_IAnsorgNS0

--- src/utility/Coordinates/Coordinates_AnsorgNS.c
+++ src/utility/Coordinates/Coordinates_AnsorgNS.c
@@ -48,6 +48,49 @@
 #define RETURN_ddphidydz(dom) CALL_ALL_AnsorgNS(dom); return ddABphi[3][2][3]
 #define RETURN_ddphidzdz(dom) CALL_ALL_AnsorgNS(dom); return ddABphi[3][3][3]
 
+/* code pieces to get dx/dX */
+#define CALL_INV_AnsorgNS(dom) \
+  tBox *box = (tBox *) aux;\
+  double xyz[4];\
+  double dxyz[4][4];\
+  double ddxyz[4][4][4];\
+  xyz_dxyz_ddxyz_of_AnsorgNS_ABphi(box, ind, dom,A,B,phi, xyz, dxyz, ddxyz);
+
+#define RETINV_x(dom) CALL_INV_AnsorgNS(dom); return xyz[1]
+#define RETINV_y(dom) CALL_INV_AnsorgNS(dom); return xyz[2]
+#define RETINV_z(dom) CALL_INV_AnsorgNS(dom); return xyz[3]
+#define RETINV_dxdA(dom) CALL_INV_AnsorgNS(dom);   return dxyz[1][1]
+#define RETINV_dxdB(dom) CALL_INV_AnsorgNS(dom);   return dxyz[1][2]
+#define RETINV_dxdphi(dom) CALL_INV_AnsorgNS(dom); return dxyz[1][3]
+
+#define RETINV_dydA(dom) CALL_INV_AnsorgNS(dom);   return dxyz[2][1]
+#define RETINV_dydB(dom) CALL_INV_AnsorgNS(dom);   return dxyz[2][2]
+#define RETINV_dydphi(dom) CALL_INV_AnsorgNS(dom); return dxyz[2][3]
+
+#define RETINV_dzdA(dom) CALL_INV_AnsorgNS(dom);   return dxyz[3][1]
+#define RETINV_dzdB(dom) CALL_INV_AnsorgNS(dom);   return dxyz[3][2]
+#define RETINV_dzdphi(dom) CALL_INV_AnsorgNS(dom); return dxyz[3][3]
+
+#define RETINV_ddxdAdA(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[1][1][1]
+#define RETINV_ddxdAdB(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[1][1][2]
+#define RETINV_ddxdAdphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[1][1][3]
+#define RETINV_ddxdBdB(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[1][2][2]
+#define RETINV_ddxdBdphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[1][2][3]
+#define RETINV_ddxdphidphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[1][3][3]
+
+#define RETINV_ddydAdA(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[2][1][1]
+#define RETINV_ddydAdB(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[2][1][2]
+#define RETINV_ddydAdphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[2][1][3]
+#define RETINV_ddydBdB(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[2][2][2]
+#define RETINV_ddydBdphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[2][2][3]
+#define RETINV_ddydphidphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[2][3][3]
+
+#define RETINV_ddzdAdA(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[3][1][1]
+#define RETINV_ddzdAdB(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[3][1][2]
+#define RETINV_ddzdAdphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[3][1][3]
+#define RETINV_ddzdBdB(dom) CALL_INV_AnsorgNS(dom);   return ddxyz[3][2][2]
+#define RETINV_ddzdBdphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[3][2][3]
+#define RETINV_ddzdphidphi(dom) CALL_INV_AnsorgNS(dom); return ddxyz[3][3][3]
 
 
 /* Coord. trafo and its derivs for domain 0,1,2,3 */
@@ -159,6 +202,9 @@ void xyz_dABphi_ddABphi_of_AnsorgNS(tBox *box, int ind, int domain,
   ddXdxdx_from_dXdx_ddxdXdX(ddABphi_ddxyz, dABphi_dxyz, ddxyz_ddABphi);
 }
 
+/*************************************************************************/
+/* Start NAnsorgNS? functions                                            */
+/*************************************************************************/
 /* Coord. trafos for domain 0 */
 double   x_of_NAnsorgNS0(void *aux, int ind, double A, double B, double phi)
 { RETURN_x(0); }
@@ -439,3 +485,287 @@ double  ddphi_dydz_NAnsorgNS3(void *aux, int ind, double A, double B, double phi
 { RETURN_ddphidydz(3); }
 double  ddphi_dzdz_NAnsorgNS3(void *aux, int ind, double A, double B, double phi)
 { RETURN_ddphidzdz(3); }
+
+/*************************************************************************/
+/* Start IAnsorgNS? functions                                            */
+/*************************************************************************/
+/* Coord. trafos for domain 0 */
+double   x_of_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_x(0); }
+double   y_of_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_y(0); }
+double   z_of_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_z(0); }
+
+/* first coord. derivs for domain 0 */
+double  dx_dA_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdA(0); }
+double  dx_dB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdB(0); }
+double  dx_dp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdphi(0); }
+
+double  dy_dA_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydA(0); }
+double  dy_dB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydB(0); }
+double  dy_dp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydphi(0); }
+
+double  dz_dA_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdA(0); }
+double  dz_dB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdB(0); }
+double  dz_dp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdphi(0); }
+
+/* second coord. derivs for domain 0 */
+double  ddx_dAdA_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdA(0); }
+double  ddx_dAdB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdB(0); }
+double  ddx_dAdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdphi(0); }
+double  ddx_dBdB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdB(0); }
+double  ddx_dBdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdphi(0); }
+double  ddx_dpdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdphidphi(0); }
+
+double  ddy_dAdA_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdA(0); }
+double  ddy_dAdB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdB(0); }
+double  ddy_dAdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdphi(0); }
+double  ddy_dBdB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdB(0); }
+double  ddy_dBdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdphi(0); }
+double  ddy_dpdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydphidphi(0); }
+
+double  ddz_dAdA_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdA(0); }
+double  ddz_dAdB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdB(0); }
+double  ddz_dAdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdphi(0); }
+double  ddz_dBdB_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdB(0); }
+double  ddz_dBdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdphi(0); }
+double  ddz_dpdp_IAnsorgNS0(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdphidphi(0); }
+
+/* Coord. trafos for domain 1 */
+double   x_of_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_x(1); }
+double   y_of_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_y(1); }
+double   z_of_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_z(1); }
+
+/* first coord. derivs for domain 1 */
+double  dx_dA_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdA(1); }
+double  dx_dB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdB(1); }
+double  dx_dp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdphi(1); }
+
+double  dy_dA_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydA(1); }
+double  dy_dB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydB(1); }
+double  dy_dp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydphi(1); }
+
+double  dz_dA_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdA(1); }
+double  dz_dB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdB(1); }
+double  dz_dp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdphi(1); }
+
+/* second coord. derivs for domain 1 */
+double  ddx_dAdA_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdA(1); }
+double  ddx_dAdB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdB(1); }
+double  ddx_dAdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdphi(1); }
+double  ddx_dBdB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdB(1); }
+double  ddx_dBdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdphi(1); }
+double  ddx_dpdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdphidphi(1); }
+
+double  ddy_dAdA_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdA(1); }
+double  ddy_dAdB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdB(1); }
+double  ddy_dAdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdphi(1); }
+double  ddy_dBdB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdB(1); }
+double  ddy_dBdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdphi(1); }
+double  ddy_dpdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydphidphi(1); }
+
+double  ddz_dAdA_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdA(1); }
+double  ddz_dAdB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdB(1); }
+double  ddz_dAdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdphi(1); }
+double  ddz_dBdB_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdB(1); }
+double  ddz_dBdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdphi(1); }
+double  ddz_dpdp_IAnsorgNS1(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdphidphi(1); }
+
+/* Coord. trafos for domain 2 */
+double   x_of_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_x(2); }
+double   y_of_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_y(2); }
+double   z_of_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_z(2); }
+
+/* first coord. derivs for domain 2 */
+double  dx_dA_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdA(2); }
+double  dx_dB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdB(2); }
+double  dx_dp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdphi(2); }
+
+double  dy_dA_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydA(2); }
+double  dy_dB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydB(2); }
+double  dy_dp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydphi(2); }
+
+double  dz_dA_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdA(2); }
+double  dz_dB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdB(2); }
+double  dz_dp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdphi(2); }
+
+/* second coord. derivs for domain 2 */
+double  ddx_dAdA_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdA(2); }
+double  ddx_dAdB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdB(2); }
+double  ddx_dAdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdphi(2); }
+double  ddx_dBdB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdB(2); }
+double  ddx_dBdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdphi(2); }
+double  ddx_dpdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdphidphi(2); }
+
+double  ddy_dAdA_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdA(2); }
+double  ddy_dAdB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdB(2); }
+double  ddy_dAdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdphi(2); }
+double  ddy_dBdB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdB(2); }
+double  ddy_dBdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdphi(2); }
+double  ddy_dpdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydphidphi(2); }
+
+double  ddz_dAdA_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdA(2); }
+double  ddz_dAdB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdB(2); }
+double  ddz_dAdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdphi(2); }
+double  ddz_dBdB_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdB(2); }
+double  ddz_dBdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdphi(2); }
+double  ddz_dpdp_IAnsorgNS2(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdphidphi(2); }
+
+
+/* Coord. trafos for domain 3 */
+double   x_of_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_x(3); }
+double   y_of_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_y(3); }
+double   z_of_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_z(3); }
+
+/* first coord. derivs for domain 3 */
+double  dx_dA_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdA(3); }
+double  dx_dB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdB(3); }
+double  dx_dp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dxdphi(3); }
+
+double  dy_dA_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydA(3); }
+double  dy_dB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydB(3); }
+double  dy_dp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dydphi(3); }
+
+double  dz_dA_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdA(3); }
+double  dz_dB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdB(3); }
+double  dz_dp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_dzdphi(3); }
+
+/* second coord. derivs for domain 3 */
+double  ddx_dAdA_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdA(3); }
+double  ddx_dAdB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdB(3); }
+double  ddx_dAdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdAdphi(3); }
+double  ddx_dBdB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdB(3); }
+double  ddx_dBdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdBdphi(3); }
+double  ddx_dpdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddxdphidphi(3); }
+
+double  ddy_dAdA_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdA(3); }
+double  ddy_dAdB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdB(3); }
+double  ddy_dAdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydAdphi(3); }
+double  ddy_dBdB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdB(3); }
+double  ddy_dBdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydBdphi(3); }
+double  ddy_dpdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddydphidphi(3); }
+
+double  ddz_dAdA_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdA(3); }
+double  ddz_dAdB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdB(3); }
+double  ddz_dAdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdAdphi(3); }
+double  ddz_dBdB_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdB(3); }
+double  ddz_dBdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdBdphi(3); }
+double  ddz_dpdp_IAnsorgNS3(void *aux, int ind, double A, double B, double phi)
+{ RETINV_ddzdphidphi(3); }
