3c64cfb7786c93c249b8b63eea6497145f117a35
Wolfgang <wolf@fau.edu>
2018-01-27 17:41:34 -0500

    add more CubedSphere choices depending on nboxes

--- src/physics/TestDerivs/Setup_CubedSpheres.c
+++ src/physics/TestDerivs/Setup_CubedSpheres.c
@@ -17,8 +17,26 @@ int Setup_CubedSpheres_initboxes(tGrid *grid)
     xc[1] = 1.0;
 
     /* test cubed spheres */
-    arrange_1box12CubSph_into_full_cube(grid, 0, xc, 0.2,0.4,0.6);
+    switch(grid->nboxes)
+    {
+      case 13:
+        arrange_1box12CubSph_into_full_cube(grid, 0, xc, 0.2,0.4,0.6);
+        break;
+      case 26:
+        two_full_cubes_touching_at_x0(grid, 0, xc[1], 0.2,0.4, 0.3,0.5);
+        break;
+      case 32:
+        sphere_around_two_full_cubes_touching_at_x0(grid, 0, xc[1],
+                                                    0.2,0.4, 0.3,0.5, 4.0);
+        break;
+      case 38:
+        two_spheres_around_two_full_cubes(grid, 0, xc[1],
+                                          0.2,0.4, 0.3,0.5, 4.0,40.0);
+        break;
+      default:
+        errorexit("nboxes should be 13, 26, 32, or 38");
+    }
   }
-parameterio_write_current_pars(grid);
+  parameterio_write_current_pars(grid);
   return 0;
 }
