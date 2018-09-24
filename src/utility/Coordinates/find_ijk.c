a6c9240a93b208222a0e906ae2548d7bb7edee7b
wolf <wolf@fau.edu>
2018-09-24 13:18:52 -0400

    fix bug

--- src/utility/Coordinates/find_ijk.c
+++ src/utility/Coordinates/find_ijk.c
@@ -9,8 +9,9 @@
 int find_i_Of_X(tBox *box, double X0)
 {
   double *X = box->v[Ind("X")];
+  int n1 = box->n1;
   int i;
-  for(i=0; i<box->n1; i++)
+  for(i=0; i<n1; i++)
     if(dequal(X[i], X0)) return i;
   return -1;
 }
@@ -19,9 +20,11 @@ int find_i_Of_X(tBox *box, double X0)
 int find_j_Of_Y(tBox *box, double Y0)
 {
   double *Y = box->v[Ind("Y")];
+  int n1 = box->n1;
+  int n2 = box->n2;
   int j;
-  for(j=0; j<box->n2; j++)
-    if(dequal(Y[j], Y0)) return j;
+  for(j=0; j<n2; j++)
+    if(dequal(Y[j*n1], Y0)) return j;
   return -1;
 }
 
@@ -29,8 +32,12 @@ int find_j_Of_Y(tBox *box, double Y0)
 int find_k_Of_Z(tBox *box, double Z0)
 {
   double *Z = box->v[Ind("Z")];
+  int n1 = box->n1;
+  int n2 = box->n2;
+  int n3 = box->n3;
+  int n1n2 = n1*n2;
   int k;
-  for(k=0; k<box->n3; k++)
-    if(dequal(Z[k], Z0)) return k;
+  for(k=0; k<n3; k++)
+    if(dequal(Z[k*n1n2], Z0)) return k;
   return -1;
 }
