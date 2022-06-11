2c40e5949ce8ab15342318dbbafce9de983e20af
Wolfgang Tichy <wolf@fau.edu>
2022-06-11 19:22:47 -0400

    more comments

--- src/physics/Poisson3/sgrid_Poisson3.c
+++ src/physics/Poisson3/sgrid_Poisson3.c
@@ -17,14 +17,14 @@ int sgrid_Poisson3()
   AddFun(ANALYZE, Poisson3_analyze, "compute error");
 
   /* variables */
-  AddVar("Poisson3_Psi",     "", "function that satisfies Eq 1: "
-         "Chi Psixx + Psiyy + Chi Chi Psizz[i] = rh1");
+  AddVar("Poisson3_Psi",     "", "field that satisfies Eq 1: "
+         "Chi Psixx + Psiyy + Chi Chi Psizz = rh1");
   AddVar("Poisson3_Psi",     "i",    "1st deriv of Psi");
   AddVar("Poisson3_Psi",     "(ij)", "2nd deriv of Psi");
   AddVar("Poisson3_rh1",     "",     "Source on RHS1");
   AddVar("Poisson3_Err_Psi", "",     "Error in Psi");
 
-  AddVar("Poisson3_Chi",     "", "another function that satisfies Eq 2: "
+  AddVar("Poisson3_Chi",     "", "another field that satisfies Eq 2: "
          "Chixx + Psix Chiyy + Chizz = rh2");
   AddVar("Poisson3_Chi",     "i",    "1st deriv of Chi");
   AddVar("Poisson3_Chi",     "(ij)", "2nd deriv of Chi");
