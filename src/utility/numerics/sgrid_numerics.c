aaab4fc4824385e921c81921ecf746160340f7a8
Wolfgang Tichy <wolf@fau.edu>
2022-06-16 11:55:00 -0400

    more files for numerics

---
+++ src/utility/numerics/sgrid_numerics.c
@@ -0,0 +1,23 @@
+/* sgrid_numerics.c */
+/* Wolfgang Tichy 6/2019 */
+
+#include "sgrid.h"
+#include "numerics.h"
+
+
+
+int nmesh_numerics(void)
+{
+  printf("Adding numerics\n");
+
+  /* functions */
+  //AddFun(OUTPUT, mesh_numerics);
+
+  /* variables */
+  //AddAuxVar("numerics_temp1", "", "temporary variable(for vol. integrals)");
+
+  /* parameters */
+  //AddPar("0dnumerics", "", "variables to numerics norms for");
+
+  return 0;
+}
