9a8c2adfeba768a1ad789e85b39b80d8341cce7c
Wolfgang Tichy <wolf@fau.edu>
2022-06-16 09:48:34 -0400

    add rt_brak.c

---
+++ src/utility/numerics/rt_brak.c
@@ -0,0 +1,48 @@
+/* (c) Wolfgang Tichy 6.6.2022 */
+
+#include <stdio.h>
+#include <math.h>
+
+/* find a bracket [x1,x2] that contains a root of func(x, par)
+   returns number of tries j    if ok
+   returns <0                   if failure */
+#define FAC 1.6
+#define NTRIES 50
+int rt_brak(double (*func)(double x, void *par),
+            double *x1, double *x2, void *par)
+{
+  int j;
+  double f1,f2;
+
+  if (*x1 == *x2)
+  {
+    printf("Bad initial range in rt_brak\n");
+    return -1;
+  }
+
+  f1 = (*func)(*x1, par);
+  f2 = (*func)(*x2, par);
+
+  for(j=1; j<=NTRIES; j++)
+  {
+    /* return if we have a bracket */
+    if(f1*f2 < 0.) return j;
+
+    if(fabs(f1) < fabs(f2))
+    {
+      /* move bound x1 */
+      *x1 += FAC * (*x1 - *x2);
+      f1 = (*func)(*x1, par);
+    }
+    else
+    {
+      /* move bound x2 */
+      *x2 += FAC * (*x2 - *x1);
+      f2 = (*func)(*x2, par);
+    }
+  }
+  printf("Did not find bracket in rt_brak\n");
+  return -j-1;
+}
+#undef FAC
+#undef NTRIES
