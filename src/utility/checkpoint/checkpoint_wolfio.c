db3f71140e5d5e5da2470b197c572f75813d10cb
Wolfgang <wolf@fau.edu>
2022-10-31 17:41:14 -0400

    minor

--- src/utility/checkpoint/checkpoint_wolfio.c
+++ src/utility/checkpoint/checkpoint_wolfio.c
@@ -1,7 +1,7 @@
 /* checkpoint_wolfio.c */
 /* Wolfgang Tichy 10/2022 */
 
-/* include sgrid headers and then wolfio.c to make sure the renaming in 
+/* include sgrid headers and then wolfio.c to make sure the renaming in
    src/main/main/rename_symbols.h is applied */
 #include "sgrid.h"
 #include "wolfio.c"
