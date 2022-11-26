77524d0c2a0623e99434a4c1fcafdba2f5b561f1
Wolfgang <wolf@fau.edu>
2022-11-26 14:53:12 -0500

    prepare writeC.m for Mathics

--- src/Math/MathToC/writeC.m
+++ src/Math/MathToC/writeC.m
@@ -9,7 +9,7 @@
    - write definitions of C variables
    - write equations as C assignments
    - write end of function
-*)  
+*)
 
 
 (***************************************************************************)
@@ -25,12 +25,19 @@ pr[x_] := Module[{s},
   WriteString[filepointer, x];
 ]
 
+(* write comments *)
 pr["/* "<>CFunctionFile<>" */\n"];
-pr["/* Copyright (C) 2005-2008 Wolfgang Tichy, "<>
-       ToString[Date[][[3]]]<>"."<>
-       ToString[Date[][[2]]]<>"."<>
-       ToString[Date[][[1]]]<>" */\n"];
-pr["/* Produced with Mathematica */\n"];
+pr["/* Copyright (C) 2005-2022 Wolfgang Tichy, "]
+If[$VersionNumber < 5.0,
+  pr[ToString[Date[][[3]]]<>"."<>
+     ToString[Date[][[2]]]<>"."<>
+     ToString[Date[][[1]]]];,
+  pr[ToString[DateList[][[3]]]<>"."<>
+     ToString[DateList[][[2]]]<>"."<>
+     ToString[DateList[][[1]]]];
+]
+pr[" */\n"];
+pr["/* Produced with Mathematica "<>$Version<>"*/\n"];
 pr["\n"];
 
 (* write the function head *)
@@ -40,40 +47,40 @@ BeginCFunction[];
 (***************************************************************************)
 (* write definition of C variables *)
 
-prdecvar[vars_, name_] := Module[{cvars, s, nf}, 
+prdecvar[vars_, name_] := Module[{cvars, s, nf},
   cvars = vars /. expfreeindices /. gluevar;
   For[nf = 0, nf < Length[cvars], nf++,
-    s = StringForm["double *`` = box->v[``+``];\n", 
+    s = StringForm["double *`` = box->v[``+``];\n",
                     cvars[[nf+1]], name, nf];
     pr[s];
   ];
 ]
 
-prdecvarname[vars_, name_] := Module[{cvars, s, nf}, 
+prdecvarname[vars_, name_] := Module[{cvars, s, nf},
   cvars = vars /. expfreeindices /. gluevar;
   s = StringForm["int index_`` = Ind(\"``\");\n", name, name];
   pr[s];
   For[nf = 0, nf < Length[cvars], nf++,
       (* box should not be hardwired *)
-      s = StringForm["double *`` = box->v[index_`` + ``];\n", 
+      s = StringForm["double *`` = box->v[index_`` + ``];\n",
                     cvars[[nf+1]], name, nf];
     pr[s];
   ];
 ]
 
-prdecvl[vars_, name_] := Module[{cvars, s, nf}, 
+prdecvl[vars_, name_] := Module[{cvars, s, nf},
   cvars = vars /. expfreeindices /. gluevar;
   For[nf = 0, nf < Length[cvars], nf++,
-    s = StringForm["double *`` = vlldataptr(``, box, ``);\n", 
+    s = StringForm["double *`` = vlldataptr(``, box, ``);\n",
                     cvars[[nf+1]], name, nf];
     pr[s];
   ];
 ]
 
-prdecvlindices[vars_, name_] := Module[{cvars, s, nf}, 
+prdecvlindices[vars_, name_] := Module[{cvars, s, nf},
   cvars = vars /. expfreeindices /. gluevar;
   For[nf = 0, nf < Length[cvars], nf++,
-      s = StringForm["int index_`` = (``)->index[``];\n", 
+      s = StringForm["int index_`` = (``)->index[``];\n",
                     cvars[[nf+1]], name, nf];
     pr[s];
   ];
@@ -96,7 +103,7 @@ variabledeclarations[];
 pr["\n"];
 
 (* insert custom InitializationCommands[] *)
-(* in InitializationCommands[] we can put an OpenMP loop and all 
+(* in InitializationCommands[] we can put an OpenMP loop and all
    auxvars are then thread local. *)
 InitializationCommands[];
 
@@ -107,7 +114,7 @@ writeCdef[vars, auxvars]
 (***************************************************************************)
 (* write equations *)
 
-writeCassign[e_] := Module[{x, nfields, nf, acc, one},
+writeCassignCForm[e_] := Module[{x, nfields, nf, acc, one},
 
   If[e[[1]] =!= Cif && e[[1]] =!= Cinstruction,
 
@@ -138,39 +145,143 @@ writeCassign[e_] := Module[{x, nfields, nf, acc, one},
 
   If[e[[1]] === Cif,
     If[e[[2]] === end,
-      pr["}\n"]; 
+      pr["}\n"];
       pr[StringForm["/* if (``) */\n\n\n", lastif]]
     ];
     If[e[[2]] === else,
-      pr["\n"]; 
+      pr["\n"];
       pr[StringForm["} else { /* if (!``) */\n\n", lastif]]
     ];
     If[e[[2]] =!= end && e[[2]] =!= else,
-      pr["\n\n"]; 
+      pr["\n\n"];
       pr["/* conditional */\n"];
       pr[StringForm["if (``) {\n\n", e[[2]]]];
       lastif = e[[2]]
-    ] 
+    ]
   ];
 
   (* Cinstruction: a piece of C-code *)
   If[e[[1]] === Cinstruction,
-      pr["\n"]; 
+      pr["\n"];
       pr[StringForm["``\n\n", e[[2]]]];
   ];
 
 ]
 
+(*************************************************************************)
+(* HR: Mathics 4.0.0. does not implement CForm, so we use the code from
+       BAMPS instead *)
 
+(* BAMPS version of writeCassign *)
+
+(* rules for a self-made MatlabForm, which does not exist in Mathematica *)
+(* misnomer, adopted from writeMatlab *)
+
+tomatlab[e_ == f_] := ToString[e] <> " = " <> tomatlab[f] <> ";"
+
+tomatlab[r_Rational] :=
+"(" <> ToString[N[Numerator[r]]] <> "/" <> ToString[N[Denominator[r]]] <> ")"
+
+(* was "(" <> ToString[n] <> "/" <> ToString[m] <> ")"
+   but for C, need e.g. -1/2 -> -1./2.
+*)
+(* works but strange: +-0.5000
+   tovector[Rational[n_, m_]] := "("<>ToString[N[n/m,17]]<>")" /; n/m < 0 *)
+
+tomatlab[Plus[e_, f__]] := "(" <> tomatlabplus[e, f] <> ")"
+tomatlabplus[e_, f__] := tomatlab[e] <> " + " <> tomatlabplus[f]
+tomatlabplus[e_] := tomatlab[e]
+
+tomatlab[Times[-1, e__]] := "(-" <> tomatlab[Times[e]] <> ")"
+tomatlab[Times[n_, e_]] :=
+ tomatlab[n] <> "*" <> tomatlab[e] /; NumberQ[n]
+tomatlab[Times[e_, f__]] := tomatlab[e] <> "*" <> tomatlab[Times[f]]
+
+tomatlab[Power[e_, -1]] := "1/("<>tomatlab[e]<>")"
+tomatlab[Power[e_, 2]] := tomatlab[e]<>"*"<>tomatlab[e]
+tomatlab[Power[e_, 3]] := tomatlab[e]<>"*"<>tomatlab[e]<>"*"<>tomatlab[e]
+tomatlab[Power[e_, p_]] := "Power(" <> tomatlab[e] <> "," <> tomatlab[p] <> ")"
+tomatlabpower[e_, 0.5] := "sqrt("<>tomatlab[e]<>")"
+tomatlabpower[e_,-0.5] := "(1/sqrt("<>tomatlab[e]<>"))"
+tomatlabpower[e_,p_] := "Power(" <> tomatlab[e] <> "," <> tomatlab[p] <> ")"
+tomatlab[Power[e_, p_Rational]] := tomatlabpower[e,N[p]]
+(* "(" <> tomatlab[e] <> ".^" <> "(" <> tomatlab[p] <> "))" *)
+
+(* unfinished, handle mathematica True *)
+(*
+tomatlab[Sign[e_]] := "((" <> tomatlab[e] <> " > 0) - (" <> tomatlab[e] <> " < 0))"
+*)
+
+(*fmf 02/19 implemented abs for rgrmhd module*)
+tomatlab[Abs[e_]] := "fabs("<>tomatlab[e]<>")"
+
+tomatlab[sin[e_]] := "sin(" <> tomatlab[e] <> ")"
+tomatlab[cos[e_]] := "cos(" <> tomatlab[e] <> ")"
+tomatlab[acos[e_]]:= "acos(" <> tomatlab[e] <> ")"
+tomatlab[atan2[e_, p_]] := "atan2(" <> tomatlab[e] <> "," <> tomatlab[p] <> ")"
+tomatlab[fabs[e_]]:= "fabs(" <> tomatlab[e] <> ")"
+tomatlab[exp[e_]] := "exp(" <> tomatlab[e] <> ")"
+tomatlab[tanh[e_]] := "tanh(" <> tomatlab[e] <> ")"
+tomatlab[cosh[e_]] := "cosh(" <> tomatlab[e] <> ")"
+tomatlab[log[e_]] := "log(" <> tomatlab[e] <> ")"
+tomatlab[erf[e_]] := "erf(" <> tomatlab[e] <> ")"
+
+tomatlab[e_] := ToString[e]
+
+
+(* write assignment *)
+writeCassignBAMPS[e_] := Module[{},
+
+  If[e[[1]] =!= Cif && e[[1]] =!= Cinstruction,
+    (* info *)
+    Print[CForm[e[[1]]]];
+
+    (* write lhs *)
+    pr[tomatlab[e]];
+    pr["\n\n"];
+  ];
+
+  If[e[[1]] === Cif,
+    If[e[[2]] === end,
+      pr["}\n"];
+      pr[StringForm["/* if (``) */\n\n\n", lastif]]
+    ];
+    If[e[[2]] === else,
+      pr["\n"];
+      pr[StringForm["} else { /* if (!``) */\n\n", lastif]]
+    ];
+    If[e[[2]] =!= end && e[[2]] =!= else,
+      pr["\n\n"];
+      pr["/* conditional */\n"];
+      pr[StringForm["if (``) {\n\n", e[[2]]]];
+      lastif = e[[2]]
+    ]
+  ];
+
+  (* Cinstruction: a piece of C code *)
+  If[e[[1]] === Cinstruction,
+      pr["\n"];
+      pr[StringForm["``\n\n", e[[2]]]];
+  ];
+
+];
+(*************************************************************************)
+
+(* choose writeCassign version *)
+If[ValueQ[MathicsVersion],
+   writeCassign = writeCassignBAMPS,
+   writeCassign = writeCassignCForm
+]
+
+(* use writeCassign *)
 Map[writeCassign, dif];
-
 
 (***************************************************************************)
 (* write the tail or end of the function *)
 
 EndCFunction[];
 
-(* count operations *)
+(* count operations (Note: Mathics 5 has Count) *)
 
 char = ReadList[CFunctionFile, Character];
 nmul = Count[char, "*"];
@@ -191,4 +302,3 @@ Close[filepointer];
 
 timer["after writeC.m"]
 prtimer[]
-
